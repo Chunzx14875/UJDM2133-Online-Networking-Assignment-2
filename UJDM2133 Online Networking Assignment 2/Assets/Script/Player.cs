fce6a1051006518660dca01b800d3afbce3b7826
Chunzx14875 <czx14875@1utar.my>
2022-04-20 05:47:08 +0800

    ......

--- UJDM2133 Online Networking Assignment 2/Assets/Script/Player.cs
+++ UJDM2133 Online Networking Assignment 2/Assets/Script/Player.cs
@@ -12,6 +12,8 @@ public class Player : Photon.MonoBehaviour
     public SpriteRenderer sr;
     public Text PlayerNameText;
 
+    private Vector2 moveInput;
+
     public bool isGrounded = false;
     //public Transform playerPos;
     //public float positionRadius;
@@ -22,20 +24,35 @@ public class Player : Photon.MonoBehaviour
     //private bool inAir;
 
     public float MoveSpeed;
-    public float JumpForce;
+
+    public GameObject BulletObject;
+    public Transform FirePos;
+
+    public bool DisableInput = false;
 
     private void Awake()
     {
         if(photonView.isMine)
         {
             PlayerCamera.SetActive(true);
+            PlayerNameText.text = PhotonNetwork.playerName;
         }
+        else
+        {
+            PlayerNameText.text = photonView.owner.name;
+            PlayerNameText.color = Color.cyan;
+        }
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (photonView.isMine)
+        if (photonView.isMine && !DisableInput)
         {
            CheckInput();
         }
@@ -43,22 +60,64 @@ public class Player : Photon.MonoBehaviour
 
     private void CheckInput()
     {
-        var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
-        transform.position += move * MoveSpeed * Time.deltaTime;
+        //var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+        //transform.position += move * MoveSpeed * Time.deltaTime;
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        moveInput.Normalize();
+
+        rb.velocity = moveInput * MoveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            sr.flipX = true;
+            Shoot();
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            sr.flipX = false;
+            photonView.RPC("FlipTrue", PhotonTargets.AllBuffered);
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            rb.velocity = Vector2.up * JumpForce;
+            photonView.RPC("FlipFalse", PhotonTargets.AllBuffered);
         }
+
+        if(moveInput.x != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+    private void Shoot()
+    {
+        if (sr.flipX == false)
+        {
+            GameObject obj = PhotonNetwork.Instantiate(BulletObject.name, new Vector2(FirePos.transform.position.x, FirePos.transform.position.y), Quaternion.identity, 0);
+        }
+
+        if (sr.flipX == true)
+        {
+            GameObject obj = PhotonNetwork.Instantiate(BulletObject.name, new Vector2(FirePos.transform.position.x, FirePos.transform.position.y), Quaternion.identity, 0);
+            obj.GetComponent<PhotonView>().RPC("ChangeDir_left", PhotonTargets.AllBuffered);
+        }  
+    }
+
+
+    [PunRPC]
+    private void FlipTrue()
+    {
+        sr.flipX = true;
+    }
+
+    [PunRPC]
+    private void FlipFalse()
+    {
+        sr.flipX = false;
     }
 }
